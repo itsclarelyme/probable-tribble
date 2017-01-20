e413f672fefb6cc03abbf0a78ac345b7566a1a0f
Jerome Ortega <Zerospace@Jeromes-MacBook-Pro.local>
2017-01-20 14:25:53 -0800

    Beta Commit

--- Road Trip/AddStopVC.swift
+++ Road Trip/AddStopVC.swift
@@ -32,7 +32,8 @@ class AddStopVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
 	var stop_ind = 0
 	var cur_long: Double = 0.0
 	var cur_lad: Double = 0.0
-	var trip: Int16 = 0       //NEED TO UPDATE
+	var trip_id: Int64 = 0
+	var trip_Name: String?
 	
 	@IBAction func ManuallyAddStopPressed(_ sender: UIButton) {
 		//let newstop_location
@@ -64,7 +65,6 @@ class AddStopVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
 	
 	@IBAction func StartEndTripPressed(_ sender: UIButton) {
 		//ADD A STOP
-		locationManager.startUpdatingLocation()
 		AddStop(with: "End Point", Description: "Amazing End to an Amazing Trip!", laditude: cur_lad, longitude: cur_long)
 		//ADD A MARKER
 		addMarker(with: "End Point", with: cur_lad, with: cur_long)
@@ -81,14 +81,22 @@ class AddStopVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
 			return
 			}
 			self.mapImage = snapshot.image
+			self.locationManager.stopUpdatingLocation()
 		}
-			
+		
+//		self.myEvent.picture = UIImageJPEGRepresentation(chosenImage, 1);
+//		To retrieve it from Core Data, we’ll do the opposite:
+//		
+//		self.imageView.image = [UIImage imageWithData:self.myEvent.picture];
+		
 		//REDIRECT VC
 		delegate?.endButtonPressed(by: self)
 	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
+			print(trip_id)
+			print(trip_Name)
 				self.mapView.delegate = self
         // Do any additional setup after loading the view.
         
@@ -98,17 +106,24 @@ class AddStopVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.startUpdatingLocation()
         self.mapView.showsUserLocation = true
-        
-        
+			
+			//Looks for single or multiple taps.
+				let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddStopVC.dismissKeyboard))
+			
+				view.addGestureRecognizer(tap)
+		}
+	
+		func dismissKeyboard() {
+			view.endEditing(true)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+			self.locationManager.startUpdatingLocation()
 			let location = locations.last
 			cur_lad = location!.coordinate.latitude
 			cur_long = location!.coordinate.longitude
 			//USE THE LABEL TO SHOW CORD
 			tripName.text = "lad: \(cur_lad), long: \(cur_long)"
-			print("did update location long lad: ", cur_long, cur_lad)
 			if stop_ind == 0 {
 				let start = NSEntityDescription.insertNewObject(forEntityName: "Stop", into: managedObjectContext) as! Stop
 				start.stop_name = "Start"
@@ -119,7 +134,6 @@ class AddStopVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate 
 			let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
 			let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
 			self.mapView.setRegion(region, animated: true)
-			self.locationManager.stopUpdatingLocation()
     }
 	
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
--- Road Trip/AllTripTVC.swift
+++ Road Trip/AllTripTVC.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class AllTripTVC: UITableViewController, tripSummaryVCDelegate, EndTripDelegate {
 	
 	weak var delegate: AllTripVCDelegate?
 	
-	let things = ["Hello"]
+	//INITIALZE VARIOABLES
+	var trips = [Trip]()
+	let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 	
 	@IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
 		delegate?.cancelButtonPressedDown(by: self)
@@ -29,51 +32,113 @@ class AllTripTVC: UITableViewController, tripSummaryVCDelegate, EndTripDelegate 
 			controller.delegate = self
 		}
 		else {
+			let tripInfo = newTrip()
 			let navigationController = segue.destination as! AddStopVC
 			let controller = navigationController
 			controller.delegate = self
+			controller.trip_Name = tripInfo.name
+			controller.trip_id = tripInfo.trip_id
+			
 		}
 	}
 	
 	func endButtonPressed(by controller: AddStopVC) {
 		dismiss(animated: true, completion: nil)
+		//RELOAD
+		fetchData()
 	}
 	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-				tableView.dataSource = self
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		tableView.dataSource = self
+		fetchData()
+	}
 	
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation,q return the number of sections
-        return things.count
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return things.count
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - Table view data source
+	
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// #warning Incomplete implementation, return the number of rows
+		return trips.count
+	}
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "TripCell", for: indexPath)
 		// set text label to the model that is corresponding to the row in array
-		cell.textLabel?.text = things[indexPath.row]
+		cell.textLabel?.text = trips[indexPath.row].name
 		// return cell so that Table View knows what to render in each row
 		return cell
 	}
 	
-		override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-			
+	func newTrip() -> Trip{
+		let newtrip = NSEntityDescription.insertNewObject(forEntityName: "Trip", into: managedObjectContext) as! Trip
+		if trips.count == 0{
+			newtrip.trip_id = 0
+			newtrip.name = "Awesome New Trip #1!"
+
 		}
-
-
-
+		else{
+			newtrip.trip_id = trips[trips.count - 1].trip_id + 1
+			newtrip.name = "Awesome New Trip #\(newtrip.trip_id)!"
+		}
+		
+		
+		saveTrip()
+		return newtrip
+	}
+	
+	    func AddTrip(){
+	        //trip1
+	        let trip1 = NSEntityDescription.insertNewObject(forEntityName: "Trip", into: managedObjectContext) as! Trip
+	        trip1.name = "Trip #1"
+	        trip1.trip_id = 0
+	        saveTrip()
+	        //trip2
+	        let trip2 = NSEntityDescription.insertNewObject(forEntityName: "Trip", into: managedObjectContext) as! Trip
+	        trip2.name = "Trip #2"
+	        trip2.trip_id = 1
+	        saveTrip()
+	        fetchData()
+	
+	    }
+	
+	//FOR UPDATING DATA AFTER ADDING
+	func saveTrip(){
+		if managedObjectContext.hasChanges {
+			do {
+				try managedObjectContext.save()
+				print("Success")
+			} catch {
+				print("\(error)")
+			}
+		}
+	}
+	
+	//FETCH DATA
+	func fetchData(){
+		let itemRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Trip")
+		do {
+			// get the results by executing the fetch request we made earlier
+			let results = try managedObjectContext.fetch(itemRequest)
+			// downcast the results as an array of Mission objects
+			trips = results as! [Trip]
+			// print the details of each item
+			for trip in trips {
+				print("\(trip.name)")
+			}
+		} catch {
+			// print the error if it is caught (Swift automatically saves the error in "error")
+			print("\(error)")
+		}
+		tableView.reloadData()
+	}
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
 }
